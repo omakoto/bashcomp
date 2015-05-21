441c7aaf3672b31dc716ebad9369765eb3c2b1fe
omakoto <makoto.onuki@gmail.com>
2015-05-20 22:37:12 -0700

    Remove the experimental code

--- bashcomp.go
+++ bashcomp.go
@@ -95,18 +95,3 @@ _{{ .Command }}_complete() {
 
 complete -o filenames -o bashdefault -F _{{.Command}}_complete {{.Command}}
 `
-
-// func main() {
-// 	for _, e := range os.Environ() {
-// 		pair := strings.Split(e, "=")
-// 		if strings.HasPrefix(pair[0], "COMP_") {
-// 			fmt.Fprintf(os.Stderr, "%s=%q\n", pair[0], pair[1])
-// 		}
-// 	}
-// }
-
-// Example:
-// COMP_TYPE="9"
-// COMP_LINE="abc def"
-// COMP_POINT="7"
-// COMP_KEY="9"
